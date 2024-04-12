3c5ddbb2c29c9e77389a4f40a222eae8d7f433b3
Georgi Kuzmanoff <gmkuzmanoff@gmail.com>
2024-04-12 10:22:47 +0300

    Tests for RadioViewModel.cs DONE!

---
+++ TestProject/RadioViewModelTests.cs
@@ -0,0 +1,78 @@
+﻿using StreamOr.Core.Models.Radio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject
+{
+    public class RadioViewModelTests
+    {
+        private RadioViewModel model;
+        private string[] images;
+
+        [SetUp]
+        public void Setup()
+        {
+            model = new RadioViewModel();
+            images = new[] { "https://gmkuzmanoff.free.bg/images/STREAMOR/P1.jpg",
+                "https://gmkuzmanoff.free.bg/images/STREAMOR/P2.jpg",
+                "https://gmkuzmanoff.free.bg/images/STREAMOR/P3.jpg",
+                "https://gmkuzmanoff.free.bg/images/STREAMOR/P4.jpg",
+                "https://gmkuzmanoff.free.bg/images/STREAMOR/P5.jpg",
+                "https://gmkuzmanoff.free.bg/images/STREAMOR/P6.jpg"};
+        }
+
+        [Test]
+        [TestCase(null)]
+        [TestCase("")]
+        [TestCase(" ")]
+        public void Test_Title(string? title)
+        {
+            model.Title = title;
+            Assert.That(model.Title, Is.EqualTo("Unknown title"), 
+                $"Model title != {model.Title}!");
+        }
+
+        [Test]
+        [TestCase(null)]
+        [TestCase("")]
+        [TestCase(" ")]
+        public void Test_LogoUrl(string? logoUrl)
+        {
+            model.LogoUrl = logoUrl;
+            Assert.That(model.LogoUrl, Is.AnyOf(images),
+                "Model logo is empty!");
+        }
+
+        [Test]
+        [TestCase(null)]
+        [TestCase("")]
+        [TestCase(" ")]
+        public void Test_Genre(string? genre)
+        {
+            model.Genre = genre;
+            Assert.That(model.Genre, Is.EqualTo("various"),
+                $"Model genre != {model.Genre}!");
+        }
+
+        [Test]
+        [TestCase(null)]
+        [TestCase("")]
+        [TestCase(" ")]
+        public void Test_Description(string? description)
+        {
+            model.Genre = "";
+            model.Description = description;
+            Assert.That(model.Description, Is.EqualTo($"The best {model.Genre} music in the world!"),
+                $"Model description != The best {model.Genre} music in the world!");
+        }
+
+        [TearDown]
+        public void Teardown()
+        {
+            model = null!;
+        }
+    }
+}
